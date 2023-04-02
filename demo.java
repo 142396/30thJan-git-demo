58d41cd96501bfbf19ccc6f663c37fe855a29f88
142396 <125986126+142396@users.noreply.github.com>
2023-04-02 12:11:30 +0530

    Update demo.java
    Modified demo.java on remote

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 
 public class Demo {
     public static void main(String[] args) {
-        // DevB code for Hello World
+        // modified demo.java on remote
 	   system.out.println("Hello World");
         // Print statement for Devops
 	   system.out.println("Welcome to Devops");
